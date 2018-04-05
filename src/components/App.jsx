2218f4773b86dc58f2822476471c49ba4411ae3c
Subodh Dahal <contact.subodhdahal@gmail.com>
2018-04-05 11:52:59 +0200

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Digits from './Digits'
+
+export default class App extends React.Component {
+    render() {
+        return <div>
+            <Digits/>
+        </div>
+    }
+}
---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import {devToolsEnhancer} from 'redux-devtools-extension'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import reducer from './reducer'
+import './style.css'
+import App from './components/App'
+
+const store = createStore(
+    reducer,
+    devToolsEnhancer())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <div>
+            <App/>
+        </div>
+    </Provider>,
+    document.getElementById('main'))
